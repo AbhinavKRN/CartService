f63a94f6b16a88134af26d0f09f4220f91431693
AbhinavKRNarayan <115357208+AbhinavKRN@users.noreply.github.com>
2024-03-12 16:40:38 +0530

    Add files via upload

---
+++ src/test/java/com/abhinav/cartservices/CartServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinav.cartservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
